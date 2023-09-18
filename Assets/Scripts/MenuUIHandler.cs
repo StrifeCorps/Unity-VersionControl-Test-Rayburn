c4f541d2ab8e4656285259b3703de52b98d6fa17
StrifeCorps <strifecorps@gmail.com>
2023-09-18 18:30:55 -0500

    Added Color Save/Load and scene persistence
    Color can now be selected and saved/loaded from the main menu for the vehicles in game. Json utility used for storing/loading.

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -17,6 +17,18 @@ public class MenuUIHandler : MonoBehaviour
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
+        MainManager.Instance.TeamColor = color;
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.Save();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.Load();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
     
     private void Start()
@@ -24,6 +36,8 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     private void StartNew()
@@ -33,6 +47,8 @@ public class MenuUIHandler : MonoBehaviour
 
     public void ExitGame()
     {
+        MainManager.Instance.Save();
+
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
